cf0fe4b5e75c42534a558401ffdfd0b2cd15e801
kaio Guilherme <kaioguilherme444@gmail.com>
2022-02-07 20:47:32 -0400

    Criação_Do_projeto

---
+++ Res1_agenda_estudantil/src/App.java
@@ -0,0 +1,14 @@
+/************************************************************
+| TITULO: Classe doguinhos                                  |
+| AUTORES: Kaio Guilherme, Lucas Anderson, Wandressa Reis   |
+| DATA DE INICIO: 07/02/2022                                |
+| DATA DE TERMINO: 00/00/0000                               |
+| PROPOSITO:                                                |   
+| INSTITUIÇÃO: Uiversidade federal De Roraima               |
+************************************************************/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
