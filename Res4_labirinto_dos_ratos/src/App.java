701aa9159878b25996135bc31a2f7ee4f744deea
kaio Guilherme <kaioguilherme444@gmail.com>
2022-03-01 12:49:57 -0400

    v0.1
    Implementação Da  classe mapa

--- Res4_labirinto_dos_ratos/src/App.java
+++ Res4_labirinto_dos_ratos/src/App.java
@@ -1,3 +1,5 @@
+import Classes.Mapa;
+
 /************************************************************
 | TITULO: Classe doguinhos                                  |
 | AUTORES: Kaio Guilherme, Lucas Anderson, Wandressa Reis   |
@@ -9,6 +11,41 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Mapa mapa = new Mapa();
+        mapa.add_Entrada(0, 1);
+        mapa.add_Saida(8, 26);
+
+        //desenhando mapa
+        //linha 1
+        mapa.set_mapa(1, 2, '*');
+        mapa.set_mapa(1, 8, '*');
+        mapa.set_mapa(1, 21, '*');
+        mapa.set_mapa(1, 25, '*');
+
+        //linha 2
+        mapa.set_mapa(2, 2, '*');
+        mapa.set_mapa(2, 3, '*');
+        mapa.set_mapa(2, 4, '*');
+        mapa.set_mapa(2, 5, '*');
+        mapa.set_mapa(2, 6, '*');
+        mapa.set_mapa(2, 10, '*');
+        mapa.set_mapa(2, 14, '*');
+        mapa.set_mapa(2, 15, '*');
+        mapa.set_mapa(2, 16, '*');
+        mapa.set_mapa(2, 17, '*');
+        mapa.set_mapa(2, 18, '*');
+        mapa.set_mapa(2, 19, '*');
+        mapa.set_mapa(2, 20, '*');
+        mapa.set_mapa(2, 21, '*');
+        mapa.set_mapa(2, 23, '*');
+        mapa.set_mapa(2, 24, '*');
+        mapa.set_mapa(2, 25, '*');
+        mapa.set_mapa(2, 26, '*');
+        mapa.set_mapa(2, 28, '*');
+        mapa.set_mapa(2, 30, '*');
+
+
+        mapa.print_mapa();
     }
 }
---
+++ Res4_labirinto_dos_ratos/src/Classes/Mapa.java
@@ -0,0 +1,115 @@
+package Classes;
+
+public class Mapa {
+    // Atributos
+    private char[][] mapa;
+    private int[] Entrada;
+    private int[] Saida;
+
+    // Construtor
+    public Mapa() {
+        this.mapa = new char[9][33];
+        this.Entrada = new int[2];
+        this.Saida = new int[2];
+        this.Fechar_linha(0);
+        this.Fechar_linha(8);
+
+        //coluna 1
+        this.mapa[1][0] = '*';
+        this.mapa[2][0] = '*';
+        this.mapa[3][0] = '*';
+        this.mapa[4][0] = '*';
+        this.mapa[5][0] = '*';
+        this.mapa[6][0] = '*';
+        this.mapa[7][0] = '*';
+        
+        
+        //coluna 32
+        this.mapa[1][32] = '*';
+        this.mapa[2][32] = '*';
+        this.mapa[3][32] = '*';
+        this.mapa[4][32] = '*';
+        this.mapa[5][32] = '*';
+        this.mapa[6][32] = '*';
+        this.mapa[7][32] = '*';
+        
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < 32; j++) {
+                this.mapa[i][j]= '@';
+            }
+            
+        }
+
+        
+
+    }
+    // get
+
+    public char[][] getMapa() {
+        return this.mapa;
+    }
+
+    public int[] getEntrada() {
+        return this.Entrada;
+    }
+
+    public int[] getSaida() {
+        return this.Saida;
+    }
+
+    // Metodos
+    private void Fechar_linha(int linha) {
+        for (int i = 0; i < 33; i++) {
+            this.mapa[linha][i] = '*';
+        }
+    }
+
+    public void add_posição(int x, int y, char nome) {
+        this.mapa[x][y] = nome;
+    }
+
+    public void set_mapa(int x, int y, char objeto){
+        if (x != 0 && x != 8 && y != 0 && y != 32) {
+            this.mapa[x][y] = objeto;
+        } else {
+            System.out.println("Posição de objeto inválida");
+        }
+    }
+
+    public void add_Entrada(int x, int y) {
+        if((x == 0 || x == 8 || y == 0 || y == 32)){
+            this.Entrada[0] = x;
+            this.Entrada[1] = y;
+            this.mapa[x][y] = 'E';   
+            
+        }else{
+            System.out.println("Posição de entrada inválida");
+        }
+        
+    }
+
+    public void add_Saida(int x, int y) {
+        if((x == 0 || x == 8 || y == 0 || y == 32)){
+            this.Entrada[0] = x;
+            this.Entrada[1] = y;
+            this.mapa[x][y] = 'S';
+
+        } else {
+            System.out.println("Posição de saida inválida");
+        }
+    }
+
+    public void print_mapa() {
+        System.out.print("\033[H\033[2J");
+        System.out.println("|====== Labirinto dos ratos ======|");
+        for (int i = 0; i < 9; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 33; j++) {
+                System.out.format("%c",this.mapa[i][j]);
+            }
+            System.out.printf("|\n");
+        }
+        System.out.println("|============== Java =============|");
+    }
+
+}
